551350e4561533c9504741cc133513cf6dde26e1
shubham-mali7 <shubhammali31299@gmail.com>
2024-03-28 16:52:07 -0700

    completed CRUD

---
+++ backend/index.js
@@ -0,0 +1,78 @@
+import express, { urlencoded } from "express";
+import mysql from "mysql2";
+import cors from "cors";
+
+const app = express();
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Shubhammali31299@007",
+  database: "test",
+});
+
+// If there is authentication problem
+// ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Shubhammali31299@007' OR
+// Or uninstalled mysql, and installed mysql2 (npm i mysql2)
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json("hello");
+});
+
+app.get("/books", (req, res) => {
+  let q = "SELECT * FROM test.books";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.post("/books", (req, res) => {
+  const q = "INSERT INTO books (`title`, `desc`, `price`, `cover`) VALUES (?)";
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
+  ];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Book has been created successfully");
+  });
+});
+
+app.delete("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q = "DELETE FROM BOOKS WHERE id = ?";
+
+  db.query(q, [bookId], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Book has been deleted successfully");
+  });
+});
+
+app.put("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q =
+    "UPDATE books SET `title` = ?, `desc` = ?, `price` = ?, `cover` = ? WHERE id = ?";
+
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
+  ];
+
+  db.query(q, [...values, bookId], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Book has been updated successfully");
+  });
+});
+
+app.listen(8800, () => {
+  console.log("Connected to backend!!");
+});
